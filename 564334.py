25de28a5250e8cef2f4b54522694e174f7bd8e25
katayo6 <62605319+katayo6@users.noreply.github.com>
2021-07-07 19:09:50 +0500

    Update 564334.py

--- 564334.py
+++ 564334.py
@@ -4,33 +4,36 @@ import telebot
 import time
 config_dict = get_default_config()
 config_dict['language'] = 'ru' 
-
+botToken = '1800442691:AAGoUT24m4S0LQWA8o1saKRPuzEK0gc-aRU'
 bot = telebot.TeleBot(botToken)
 owm = OWM('9ba44acc75da89cee8a654c527d809ec', config_dict)
 @bot.message_handler(content_types=['text'])
 def send_message(message):
-	  try:
-	      mgr = owm.weather_manager()
-	      observation = mgr.weather_at_place('Kurgan,RU')
-	      weather = mgr.weather_at_place('Kurgan,RU').weather
-	      w = observation.weather
-	      temp = weather.temperature('celsius')['temp'] + 2
-	      wind = w.wind(unit='meters_sec')
-	      answer = "В Кургане сейчас " + weather.detailed_status + '\n\n'
-	      answer +='Ветер: ' + str(int(wind['speed'])) + ' м/с' + '\n\n'
-	      answer += 'Температура на данный момент: '  + str(int(temp))  + "°С" + '\n\n'
-	      if temp < 20:
-			     answer += 'Надень что-нибудь потеплее'
-	      elif temp > 20:
-		             answer += 'Надень что-нибудь полегче'
-	      print(time.ctime(), "User id:", message.from_user.id)
-	      print(time.ctime(), "Message:", message.text.title())
-								
-	  except Exception:
+	try:
+		mgr = owm.weather_manager()
+		observation = mgr.weather_at_place('Kurgan,RU')
+		weather = mgr.weather_at_place('Kurgan,RU').weather 
+		w = observation.weather
+		temp = weather.temperature('celsius')['temp']
+		wind = w.wind(unit='meters_sec')
+		answer = "В Кургане сейчас " + weather.detailed_status + '\n\n'
+		answer +='Ветер: ' + str(int(wind['speed'])) + ' м/с' + '\n\n'
+		answer += 'Температура на данный момент: '  + str(int(temp))  + "°С" + '\n\n'
+		if temp < 10:
+			answer += 'Надень что-нибудь потеплее'
+		elif temp > 15 and wind > 4:
+			answer += 'Надень кофту и возьми с собой ветровку'
+		elif temp > 20 and wind > 4:
+			answer += 'Надень футболку с ветровкой'
+		elif temp > 25:
+			answer += 'Можешь надеть футболку'
+		elif temp > 31:
+			answer += 'Это пизда, включай вентиляторы'
+		print(time.ctime(), "User id:", message.from_user.id)
+		print(time.ctime(), "Message:", message.text.title())
+	except Exception:
             		print(time.ctime(), "User id:", message.from_user.id)
             		print(time.ctime(), "Message:", message.text.title())
-
-	
-	  bot.send_message(message.chat.id, answer)
-
+	bot.send_message(message.chat.id, answer)
 bot.polling(none_stop=True)
+
